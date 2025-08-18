990c8ab90e2d5780a71d2becb707b8de125027c8
nyt <nyt.git@gmail.com>
2025-08-18 11:47:24 +0200

    ``` feat: add animations and sprint functionality to player movement
    - Added idle, walk, and sprint animations in `player.tscn`
    - Configured `AnimationPlayer` and set up animation transitions in `player.gd`
    - Introduced a `Sprint` input action with a deadzone in `project.godot`
    - Adjusted player speed constants for walking and sprinting
    ```

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,31 @@
 extends CharacterBody2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var speed_velocity = 300.0
+const WALK_SPEED: float                        = 100.0
+const SPRINT_SPEED: float                      = 300.0
 
-func _physics_process(delta: float) -> void:
-	var input_vector = Input.get_vector("Left", "Right", "Up", "Down")
-	
-	velocity = input_vector.normalized() * speed_velocity
 
+func _physics_process(_delta: float) -> void:
+	var input_vector: Vector2 = Input.get_vector("Left", "Right", "Up", "Down")
+	var is_sprinting: bool    = Input.is_action_pressed("Sprint")
+	var current_speed: float  = SPRINT_SPEED if is_sprinting else WALK_SPEED
+
+	# Applique la vélocité
+	velocity = input_vector.normalized() * current_speed
+
+	# Flip sprite
 	if velocity.x != 0:
 		sprite_2d.flip_h = velocity.x < 0
 
-	# Applique le mouvement
+	# Gestion des animations
+	if input_vector == Vector2.ZERO:
+		animation_player.play("idle")
+	else:
+		if is_sprinting:
+			animation_player.play("sprint")
+		else:
+			animation_player.play("walk")
+
 	move_and_slide()
