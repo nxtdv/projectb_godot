670829284c8b9e94e5941dd42e3c9ad587ce616a
nyt <nyt.git@gmail.com>
2025-08-19 19:40:06 +0200

    feat(player): add attack input and behavior
    - implemented attack mechanics triggered on input action "Attack"
    - updated AnimationPlayer to handle attack animation completion
    - added `is_attacking` flag to manage state and prevent movement during attack
    - registered new input action "Attack" in project settings

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,24 @@ extends CharacterBody2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-const WALK_SPEED: float   = 100.0
-const SPRINT_SPEED: float = 300.0
+const WALK_SPEED: float                        = 100.0
+const SPRINT_SPEED: float                      = 300.0
+var is_attacking: bool                         = false
+
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(_on_animation_player_animation_finished)
 
 
 func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Attack") and not is_attacking:
+		_start_attack()
+		return
+
+	# If the player is attacking, we don't process movement
+	if is_attacking:
+		return
+
 	var input_vector: Vector2 = Input.get_vector("Left", "Right", "Up", "Down")
 	var is_sprinting: bool    = Input.is_action_pressed("Sprint")
 	var current_speed: float  = SPRINT_SPEED if is_sprinting else WALK_SPEED
@@ -28,3 +41,14 @@ func _physics_process(_delta: float) -> void:
 			animation_player.play("walk")
 
 	move_and_slide()
+
+
+func _start_attack() -> void:
+	is_attacking = true
+	velocity = Vector2.ZERO  # We stop moving when attacking
+	animation_player.play("attack")
+
+
+func _on_animation_player_animation_finished(animation_name: String) -> void:
+	if animation_name == "attack":
+		is_attacking = false
