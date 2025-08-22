3423771f374a40386f76646e1c0172414cc16710
nyt <nyt.git@gmail.com>
2025-08-22 17:53:31 +0200

    refactor(dialogue): remove speaker-related properties and customization
    - deleted speaker_parent node and associated sub-nodes from dialogue system scene
    - removed speaker-related export variables from DialogueText and DialogueChoice scripts
    - added new properties for text speed, sound, and volume customization in DialogueChoice script

--- scripts/dialogue_choice_resource.gd
+++ scripts/dialogue_choice_resource.gd
@@ -1,11 +1,12 @@
 ﻿extends DE
 class_name DialogueChoice
 
-@export var speaker_name: String
-@export var speaker_img: Texture
-@export var speaker_img_Hframes: int = 1
-@export var speaker_img_select_frames: int = 0
-
 @export_multiline var text: String
 @export var choice_text: Array[String]
-@export var choice_function_call: Array[DialogueFunction] 
+@export var choice_function_call: Array[DialogueFunction]
+
+@export_range(0.1, 30.0, 0.1) var text_speed: float = 18.0
+@export var text_sound: AudioStream
+@export var text_volume_db: int = -8
+@export var text_volume_pitch_min: float = 0.85
+@export var text_volume_pitch_max: float = 1.15
--- scripts/dialogue_text_resource.gd
+++ scripts/dialogue_text_resource.gd
@@ -1,11 +1,6 @@
 extends DE
 class_name DialogueText
 
-@export var speaker_name: String
-@export var speaker_img: Texture
-@export var speaker_img_Hframes: int = 1
-@export var speaker_img_rest_frames: int = 0
-
 @export_multiline var text: String
 
 @export_range(0.1, 30.0, 0.1) var text_speed: float = 1.0
